d62b583830ab40daf1a49923b6bab16194f8ae1a
PedroLara14 <88643540+PedroLara14@users.noreply.github.com>
2021-10-30 06:32:04 -0500

    Add files via upload

---
+++ Taller 4 Algoritmos Pedro Lara/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace App
+{
+    class Programa
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese una palabra ");
+            String palabra = Console.ReadLine();
+            if (Verificacion_PalabraPalindroma(palabra)) Console.WriteLine("Es palíndroma");
+            else Console.WriteLine("NO es palíndroma");
+        }
+
+        static Boolean Verificacion_PalabraPalindroma(String cadena)
+        {
+            if (cadena.Length < 2) return true;
+            if (cadena[0] == cadena[cadena.Length - 1]) return Verificacion_PalabraPalindroma(cadena.Substring(1, cadena.Length - 2));
+            return false;
+        }
+    }
+}
